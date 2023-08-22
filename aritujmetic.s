6fb73e2d2663fe40333d2a491b58a14145c3c7f2
ASHUTOSH VISHWAKARMA <ashutoshvishwakarma208@outlook.com>
2023-08-22 21:33:19 +0530

    Basix code

--- aritujmetic.s
+++ aritujmetic.s
@@ -1,7 +1,16 @@
 .global _start
 
 _start:
-    MOV R0,#10
-    MOV R1,#15
-    ADD R2,R1,R0 //ADD
-    SUBS R3,R0,R1
+   
+    ldr R0, = list
+    ldr R1, [R0]
+    ldr R2, [R0,#4]
+    ADDS R3,R2,R1  //ADD with CPSR on
+    ADC R3,R11      //ADD cary too
+    mov R7,#1
+    swi 0
+
+
+.data 
+list:
+    .word 9,2,3,4,5
