82a3eb0e6e341cd191e9cd7f3067df4115b5b63e
ASHUTOSH VISHWAKARMA <ashutoshvishwakarma208@outlook.com>
2024-04-17 14:18:42 +0530

    Added

--- aritujmetic.s
+++ aritujmetic.s
@@ -1,16 +1,11 @@
-.global _start
+.global fxn
 
-_start:
+fxn:
+    mov R0,#2
+    mov R1,#-5
+    bal fxn2
+    adds r0,r1,R0
    
-    ldr R0, = list
-    ldr R1, [R0]
-    ldr R2, [R0,#4]
-    ADDS R3,R2,R1  //ADD with CPSR on
-    ADC R3,R11      //ADD cary too
-    mov R7,#1
+
+fxn2:
     swi 0
-
-
-.data 
-list:
-    .word 9,2,3,4,5
